f34407ed046d76fe32e6160ad478c13622114088
Leo Frivold <leofri06@gvgs.no>
2025-09-17 12:57:33 +0200

    Lagt til at Gjennestadlogo hører på dark mode

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function darkmode() {
     document.body.classList.toggle("darkmode");
     const icon = document.getElementById("darkmodeicon");
+    const logo = document.getElementById("gjennestadlogo");
     if (document.body.classList.contains("darkmode")) {
         icon.src="images/icons/sun_white.svg";
+        logo.src="images/gjennestadlogo_white.png";
     }
     else {
         icon.src="images/icons/sun_black.svg";
+        logo.src="images/gjennestadlogo_black.png";
     }
     localStorage.setItem("darkMode", document.body.classList.contains("darkmode")); // Brukte litt autofill, men forstår konseptet
 }
