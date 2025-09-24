5733fc3a5f860955f1063795e123eeed8932568b
Leo Frivold <leofri06@gvgs.no>
2025-09-24 09:50:35 +0200

    Lagt til åpning av Prosjekt-elementer

--- script.js
+++ script.js
@@ -27,3 +27,33 @@ document.querySelectorAll("a").forEach(link => {
 window.addEventListener("load", () => {
     document.body.classList.add("fade-in");
 });
+
+function scale(ID, imgSrc, title, desc) {
+    localStorage.clear();
+    localStorage.setItem("prosjektImg", imgSrc);
+    localStorage.setItem("prosjektTitle", title);
+    localStorage.setItem("prosjektDesc", desc);    
+    document.getElementById(ID).classList.add("scale");
+    setTimeout(() => {
+        document.body.classList.add("fade-out");
+        setTimeout(() => {
+            window.location = "prosjektDive.html"; // bytt side etter 200ms
+    }, 200);}, 200); // tid i ms
+}
+function prosjektModal(imgSrc, title, desc) {
+}
+function getProsjektModal() {
+    const imgSrc = localStorage.getItem("prosjektImg");
+    const title = localStorage.getItem("prosjektTitle");
+    const desc = localStorage.getItem("prosjektDesc");
+    document.getElementById("prosjektImg").src = imgSrc;
+    document.getElementById("prosjektTitle").innerText = title;
+    document.getElementById("prosjektDesc").innerText = desc;
+}
+window.addEventListener("load", getProsjektModal);
+    localStorage.setItem("Hello World", "This is a test");
+    document.body.classList.add("fade-in");
+    setTimeout(() => {
+        const test = localStorage.getItem("Hello World");
+        console.log(test);
+    }, 1000);
