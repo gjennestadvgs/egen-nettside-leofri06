dc3bdd169a46e786865f685be059a6dbc0ff4d32
Leo Frivold <leofri06@gvgs.no>
2025-09-24 10:42:38 +0200

    Lagt til "gå tilbake" ikon og funksjon

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ function darkmode() {
     document.body.classList.toggle("darkmode");
     const icon = document.getElementById("darkmodeicon");
     const logo = document.getElementById("gjennestadlogo");
+    const goBackButton = document.querySelector(".goBackButton");
     if (document.body.classList.contains("darkmode")) {
         icon.src="images/icons/sun_white.svg";
         logo.src="images/gjennestadlogo_white.png";
+        goBackButton.src="images/icons/goback_white.svg";
     }
     else {
         document.body.classList.add("lightmode");
         icon.src="images/icons/sun_black.svg";
         logo.src="images/gjennestadlogo_black.png";
+        goBackButton.src="images/icons/goback_black.svg";
     }
     localStorage.setItem("darkMode", document.body.classList.contains("darkmode")); // Brukte litt autofill, men forstår konseptet
 }
@@ -28,7 +31,7 @@ window.addEventListener("load", () => {
     document.body.classList.add("fade-in");
 });
 
-function scale(ID, imgSrc, title, desc) {
+function prosjektModal(ID, imgSrc, title, desc) {
     localStorage.clear();
     localStorage.setItem("prosjektImg", imgSrc);
     localStorage.setItem("prosjektTitle", title);
@@ -40,8 +43,6 @@ function scale(ID, imgSrc, title, desc) {
             window.location = "prosjektDive.html"; // bytt side etter 200ms
     }, 200);}, 200); // tid i ms
 }
-function prosjektModal(imgSrc, title, desc) {
-}
 function getProsjektModal() {
     const imgSrc = localStorage.getItem("prosjektImg");
     const title = localStorage.getItem("prosjektTitle");
@@ -51,9 +52,15 @@ function getProsjektModal() {
     document.getElementById("prosjektDesc").innerText = desc;
 }
 window.addEventListener("load", getProsjektModal);
-    localStorage.setItem("Hello World", "This is a test");
-    document.body.classList.add("fade-in");
+
+function storageClear() {
+    localStorage.clear();
+}
+
+function goBack() {
+    document.body.classList.add("fade-out");
     setTimeout(() => {
-        const test = localStorage.getItem("Hello World");
-        console.log(test);
-    }, 1000);
+        window.history.back(); // gå tilbake til forrige side etter 200ms
+        localStorage.clear();
+    }, 200); // tid i ms
+}
